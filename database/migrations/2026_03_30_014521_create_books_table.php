da5fa474704dd90f22439898898c29a369d7fff4
Isaías Víquez <253314412+isaias-viquez@users.noreply.github.com>
2026-03-29 21:10:52 -0600

    Add migrations for authors, books, and publishers tables with relationships

---
+++ database/migrations/2026_03_30_014521_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('edition');
+            $table->unsignedSmallInteger('copyright');
+            $table->string('language');
+            $table->unsignedSmallInteger('pages');
+            $table->foreignId('author_id')->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->foreignId('publisher_id')->constrained()->cascadeOnUpdate()->restrictOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
